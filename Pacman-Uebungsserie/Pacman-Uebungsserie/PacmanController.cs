a8ed5c200af9d5a1ea8885391d4d3e957519417b
gustda <d.gustorff@gmail.com>
2018-02-05 18:43:19 +0100

    Initialer Check-In

---
+++ Pacman-Uebungsserie/Pacman-Uebungsserie/PacmanController.cs
@@ -0,0 +1,28 @@
+﻿using Lernmoment.CsharpCanvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacmanUebungsserie
+{
+    class PacmanController
+    {
+        CsharpCanvas canvas;
+
+        public PacmanController(CsharpCanvas _canvas)
+        {
+            canvas = _canvas;
+            canvas.SetBackgroundColor(System.Drawing.Color.AliceBlue);
+            canvas.Draw += Canvas_Draw;
+
+            // for continous drawing
+            canvas.InitGame(5);
+        }
+               
+        private void Canvas_Draw()
+        {
+        }
+    }
+}
---
+++ Pacman-Uebungsserie/Pacman-Uebungsserie/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lernmoment.CsharpCanvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PacmanUebungsserie
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            CsharpCanvas canvas = new CsharpCanvas();
+            PacmanController launcher = new PacmanController(canvas);
+            canvas.Setup();
+        }
+    }
+}
